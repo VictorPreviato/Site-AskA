e4515890cba7ad0f510a43097aeca86137cbe115
Victor Previato <146777340+VictorPreviato@users.noreply.github.com>
2025-04-30 11:59:02 -0300

    Add files via upload

---
+++ Site-AskA/hostpaga.php
@@ -0,0 +1,3 @@
+<?php include './parciais/header.php' ; ?>
+
+<?php include './parciais/footer.php' ; ?>
---
+++ Site-AskA/parciais/footer.php
@@ -0,0 +1,8 @@
+<div id="flexfoot">
+<div id="copydiv">
+<p> © <?php date("Y"); ?> Ask A. Todos os direitos reservados</p>
+</div>
+</div>
+
+</body>
+</html>
